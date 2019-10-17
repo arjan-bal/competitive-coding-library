0d8f26ba728da5e53a67d91342e2f6e86f816f8a
Arjan Singh Bal <arjan3141@gmail.com>
2019-10-18 01:09:49 +0530

    improved sos dp

--- sos DP.cpp
+++ sos DP.cpp
@@ -1,19 +1,19 @@
+//used to find propery such as min, max, sum etc. over subsets or supersets
+
 const int N=21;
 
 int dp[1<<N][N];
 
-int cal(int mask, int idx)
-{
-    if(idx<0) return arr[mask];
-    if(dp[mask][idx]!=-1) return dp[mask][idx];
-    int ans=cal(mask, idx-1);
-    if((mask>>idx)&1) ans+=(((1<<idx)^mask), idx-1);
-    return dp[mask][idx]=ans; 
-}
-
 for(int i = 0; i<(1<<N); ++i)
     F[i] = A[i];
+
+//property over subsets
 for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
-    if(mask & (1<<i))
-        F[mask] += F[mask^(1<<i)];
+    if(mask & (1<<i)) F[mask] += F[mask^(1<<i)];
+}
+
+//property over supersets
+for(int i = 0;i < N; ++i) for(int mask = (1<<N)-1; mask >= 0; --mask){
+    if(mask & (1<<i)) continue;
+    F[mask] += F[mask|(1<<i)];
 }
