cbe9b657425c57e96024f6475e6762b66be85bb7
Arjan Singh Bal <arjan3141@gmail.com>
2019-12-01 14:36:44 +0530

    updated rolling hash

--- rolling hash.cpp
+++ rolling hash.cpp
@@ -1,29 +1,30 @@
+const int N=1e6+1, mod=1000000459;
+ll p29[N], ip29[N];
+int n;
+
+// vec is of size n, and has numbers in [1, 28]
+// if string, can replace vec[i] with s[i]-'a'+1
+
 void precal()
 {
-    p27[0]=1;
-    for(int i=1; i<N; ++i){
-        p27[i]=p27[i-1]*27LL%mod;
+    fhash[0]=vec[0];
+    p29[0]=1;
+
+    for(int i=1; i<n; ++i){
+    	p29[i]=p29[i-1]*29%mod;
+        fhash[i]=(fhash[i-1]+p29[i]*vec[i])%mod;
     }
 
-    fhash[0]=(t[0]-'a'+1);
-    for(int i=1; i<n2; ++i){
-        fhash[i]=1LL*p27[i]*(t[i]-'a'+1)%mod;
-        add(fhash[i], fhash[i-1]);
-    }
+    ip29[n-1]=modInverse(p29[n-1], mod);
 
-    ip27[N-1]=modInverse(p27[N-1], mod);
-
-    for(int i=N-2; i>=0; --i){
-        ip27[i]=27LL*ip27[i+1]%mod;
-    }
-
-    return ;
+    for(int i=n-2; i>=0; --i)
+        ip29[i]=29*ip29[i+1]%mod;
 }
 
 int calhash(int l, int r)
 {
     if(l==0) return fhash[r];
-    int ans=fhash[r];
-    sub(ans, fhash[l-1]);
-    return 1LL*ans*ip27[l]%mod;
+    int ans=fhash[r]-fhash[l-1];
+    if(ans<0) ans+=mod;
+    return ans*ip29[l]%mod;
 }
