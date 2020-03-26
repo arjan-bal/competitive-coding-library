08f25f179894d13b79b6038dbc5562d9762de950
Arjan Singh Bal <arjan3141@gmail.com>
2020-03-26 11:25:39 +0530

    Added Z2 vector space, safe hash function to umap hash

---
+++ Z2 Vector Space.cpp
@@ -0,0 +1,34 @@
+// Refference : https://codeforces.com/blog/entry/68953
+struct vectorSpace{
+	typedef int base;
+	int D, sz=0;
+	vector<base> basis;	// basis[i] keeps the mask of the vector whose f value is i
+	vectorSpace(int _D)
+	{
+		D=_D;
+		basis.resize(D, 0);
+	}
+	void insert(int mask)
+	{
+		if(sz==D) return ;
+		for(int i=D-1; i>=0; --i){			
+			if(!((mask>>i)&1)) continue;
+			if(!basis[i]){
+				basis[i]=mask;
+				++sz;
+				return ;
+			}
+			mask^=basis[i];
+		}
+	}
+	bool represent(int mask)
+	{
+		if(sz==D) return 1;
+		for(int i=D-1; i>=0; --i){
+			if(!((mask>>i)&1)) continue;
+			if(!basis[i]) return 0;
+			mask^=basis[i];
+		}
+		return 1;
+	}
+};
