344504ea5e99d000c059e617ef85cdd173574be4
Arjan Singh Bal <arjan3141@gmail.com>
2020-09-29 02:18:58 +0530

    upgrades to DSU and new applications of it

--- dsu.cpp
+++ dsu.cpp
@@ -1,18 +1,20 @@
 struct DSU{
-	int n;
+	int n, components;
 	vi par, sz;
 	int root(int x)
 	{ return x==par[x]?x:(par[x]=root(par[x]));	}
-	void merge(int x1, int x2)
+	bool merge(int x1, int x2)
 	{
 		x1=root(x1), x2=root(x2);
-		if(x1==x2) return ;
+		if(x1==x2) return false;
 		if(sz[x2]>sz[x1]) swap(x1, x2);
 		par[x2]=x1; sz[x1]+=sz[x2];
+		--components;
+		return true;
 	}
 	DSU(int _n)
 	{
-		n=_n;
+		n=components=_n;
 		sz.assign(n+1, 1);
 		par.resize(n+1);
 		iota(all(par), 0);
