d3d3904b8ed76e12e408652c3bda80cc53944445
Arjan Singh Bal <arjan3141@gmail.com>
2020-10-12 22:57:52 +0530

    add simplex

---
+++ simplex.cpp
@@ -0,0 +1,88 @@
+// attribution: https://codeforces.com/contest/1430/submission/95287878
+// should work for upto 50 variables
+/**
+ * Author: Stanford
+ * Source: Stanford Notebook
+ * License: MIT
+ * Description: Solves a general linear maximization problem: maximize $c^T x$ subject to $Ax <= b$, $x >= 0$.
+ * Returns -inf if there is no solution, inf if there are arbitrarily good solutions, or the maximum value of $c^T x$ otherwise.
+ * The input vector is set to an optimal $x$ (or in the unbounded case, an arbitrary solution fulfilling the constraints).
+ * Numerical stability is not guaranteed. For better performance, define variables such that $x = 0$ is viable.
+ * Usage:
+ * vvd A = {{1,-1}, {-1,1}, {-1,-2}};
+ * vd b = {1,1,-4}, c = {-1,-1}, x;
+ * T val = LPSolver(A, b, c).solve(x);
+ * Time: O(NM * \#pivots), where a pivot may be e.g. an edge relaxation. O(2^n) in the general case.
+ * Status: seems to work?
+ */
+
+typedef long double DOUBLE;
+typedef vector<DOUBLE> VD;
+typedef vector<VD> VVD;
+typedef vector<int> VI;
+ 
+const DOUBLE EPS = 1e-9;
+ 
+struct LPSolver {
+  int m, n;
+  VI B, N;
+  VVD D;
+ 
+  LPSolver(const VVD &A, const VD &b, const VD &c) :
+    m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
+    for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
+    for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
+    for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
+    N[n] = -1; D[m + 1][n] = 1;
+  }
+ 
+  void Pivot(int r, int s) {
+    double inv = 1.0 / D[r][s];
+    for (int i = 0; i < m + 2; i++) if (i != r)
+      for (int j = 0; j < n + 2; j++) if (j != s)
+        D[i][j] -= D[r][j] * D[i][s] * inv;
+    for (int j = 0; j < n + 2; j++) if (j != s) D[r][j] *= inv;
+    for (int i = 0; i < m + 2; i++) if (i != r) D[i][s] *= -inv;
+    D[r][s] = inv;
+    swap(B[r], N[s]);
+  }
+ 
+  bool Simplex(int phase) {
+    int x = phase == 1 ? m + 1 : m;
+    while (true) {
+      int s = -1;
+      for (int j = 0; j <= n; j++) {
+        if (phase == 2 && N[j] == -1) continue;
+        if (s == -1 || D[x][j] < D[x][s] || D[x][j] == D[x][s] && N[j] < N[s]) s = j;
+      }
+      if (D[x][s] > -EPS) return true;
+      int r = -1;
+      for (int i = 0; i < m; i++) {
+        if (D[i][s] < EPS) continue;
+        if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
+          (D[i][n + 1] / D[i][s]) == (D[r][n + 1] / D[r][s]) && B[i] < B[r]) r = i;
+      }
+      if (r == -1) return false;
+      Pivot(r, s);
+    }
+  }
+ 
+  DOUBLE Solve(VD &x) {
+    int r = 0;
+    for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
+    if (D[r][n + 1] < -EPS) {
+      Pivot(r, n);
+      if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
+      for (int i = 0; i < m; i++) if (B[i] == -1) {
+        int s = -1;
+        for (int j = 0; j <= n; j++)
+          if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
+        Pivot(i, s);
+      }
+    }
+    if (!Simplex(2)) return numeric_limits<DOUBLE>::infinity();
+    x = VD(n);
+    for (int i = 0; i < m; i++) if (B[i] < n) x[B[i]] = D[i][n + 1];
+    return D[m][n + 1];
+  }
+};
